fab11dfb9d8afa38a6c840453cb958ed8080447d
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2022-01-25 12:56:07 -0800

    update

--- google-compute-engine/jeffs-gce-ubuntu-2004/install-scripts/install-packages.sh
+++ google-compute-engine/jeffs-gce-ubuntu-2004/install-scripts/install-packages.sh
@@ -11,6 +11,7 @@ echo " "
 sudo apt-get -y install tmux
 sudo apt-get -y install htop
 sudo apt-get -y install unzip
+sudo apt-get -y install build-essential
 
 echo "******************************************** install-packages.sh (END) *"
 echo "************************************************************************"
