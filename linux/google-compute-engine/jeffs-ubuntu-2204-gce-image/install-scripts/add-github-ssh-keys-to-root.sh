6a2f54c55e87337dd25e5c8f0e94b038e0519728
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-02-24 18:23:03 -0500

    re-structured repo and adding vagrant and docker

---
+++ linux/google-compute-engine/jeffs-ubuntu-2204-gce-image/install-scripts/add-github-ssh-keys-to-root.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+# my-packer-image-builds add-github-ssh-keys-to-root.sh
+
+echo " " 
+echo "************************************************************************"
+echo "******************************* add-github-ssh-keys-to-root.sh (START) *"
+echo "You are root in /home/packer"
+echo " "
+
+echo "Make /root/.ssh"
+[ -d /root/.ssh ] || mkdir /root/.ssh
+
+echo "mv /tmp/gce-github-vm* /root/.ssh"
+cp /tmp/gce-github-vm /root/.ssh/id_rsa
+cp /tmp/gce-github-vm.pub /root/.ssh/id_rsa.pub
+echo " "
+
+echo "chmod 600 for both keys"
+chmod 600 /root/.ssh/id_rsa*
+
+echo "Start ssh agent for root"
+eval $(ssh-agent -s)
+
+echo "Put github.com fingerprint in known_hosts file"
+ssh-keyscan github.com >> /root/.ssh/known_hosts
+
+echo "Add private key to SSH authentication agent"
+ssh-add /root/.ssh/id_rsa
+
+echo "Connect to github with public key (you already put public key on github)"
+# The || : forces it to return a 0 exit code, because you get a warning first time connecting from a new ip.
+ssh -T git@github.com || :
+# ssh -i ~/.ssh/id_rsa.pub git@github.com || :
+echo " "
+
+echo "********************************* add-github-ssh-keys-to-root.sh (END) *"
+echo "************************************************************************"
+echo " "
