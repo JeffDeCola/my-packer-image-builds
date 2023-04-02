76ac01d7567bbb425c19fbcbb17231a0a8b305a0
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-04-02 04:50:48 -0400

    moved hello-go service to /usr/bin

--- google-compute-engine-images/jeffs-gce-image-ubuntu-2204/install-scripts/service-install.sh
+++ google-compute-engine-images/jeffs-gce-image-ubuntu-2204/install-scripts/service-install.sh
@@ -7,12 +7,20 @@ echo "*************************************** service-install.sh (START) *"
 echo "You are root in /home/packer"
 echo " "
 
-echo "cd into /root/src/github.com/JeffDeCola/hello-go-deploy-gce/hello-go-deploy-gce-code"
+echo "cd /home/jeff/services/hello-go-deploy-gce/hello-go-deploy-gce-code"
 cd /home/jeff/services/hello-go-deploy-gce/hello-go-deploy-gce-code || exit
 echo " "
 
-echo "Build an executable (go build), place in /root/bin"
-go build -o /root/bin/hello-go main.go
+echo "ls -lat"
+ls -lat
+echo " "
+
+echo "Build an executable (go build), place in /bin"
+go build -o /usr/bin/hello-go main.go
+echo " "
+
+echo "ls -lat /usr/bin | grep hello"
+ls -lat /usr/bin | grep hello
 echo " "
 
 echo "********************************************* service-install.sh (END) *"
